635e373c16e5ed16c829f82397e782198523b460
Lucianomin <minda.lucian@yahoo.com>
2025-04-03 11:30:59 +0300

    working try 1

---
+++ main.c
@@ -0,0 +1,13 @@
+
+#include <time.h>
+#include <SDL2/SDL.h>   /* All SDL App's need this */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc,char * args[])
+{
+    
+    return 0;
+}
