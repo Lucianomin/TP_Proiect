a21eee4e91140b2826eb9800ab4d184973a4b71c
Lucianomin <minda.lucian@yaoo.com>
2025-05-19 22:13:29 +0300

    main2.c second attempt

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpqpbpwr0d.js
+// include: /tmp/tmptpkq5_2f.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -213,21 +213,21 @@ Module['FS_createPath']("/assets", "fonts", true, true);
 
   })();
 
-// end include: /tmp/tmpqpbpwr0d.js
-// include: /tmp/tmp899dq9sn.js
+// end include: /tmp/tmptpkq5_2f.js
+// include: /tmp/tmpwsmsmbo9.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp899dq9sn.js
-// include: /tmp/tmpef2p7rdf.js
+  // end include: /tmp/tmpwsmsmbo9.js
+// include: /tmp/tmprplwohbq.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpef2p7rdf.js
+  // end include: /tmp/tmprplwohbq.js
 
 
 var arguments_ = [];
